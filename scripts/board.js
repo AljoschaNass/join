a607540ecc2f9da1caebed1128d564a476f550b7
Steffi Vengels <vengels.stefanie@gmail.com>
2025-03-25 21:20:31 +0100

    add new font (Poppins), add 2 new standard css class beginn with editDialogBoard

--- scripts/board.js
+++ scripts/board.js
@@ -18,3 +18,33 @@ function closeDialog() {
     overlayRef.classList.remove("overlayBoard");
     overlayRef.innerHTML = "";
 }
+
+function editTask() {
+    let dialogRef = document.getElementById("dialogBoard");
+    dialogRef.innerHTML = "";
+    dialogRef.innerHTML += geteditDialogTemplate();
+}
+
+function selectUrgentPriority() {
+    unselectPriority();
+    let urgentRef = document.getElementById("urgentPriority")
+    urgentRef.classList.add("urgentPriorityButtonSelected");
+}
+
+function selectMediumPriority() {
+    unselectPriority();
+    let mediumRef = document.getElementById("mediumPriority")
+    mediumRef.classList.add("mediumPriorityButtonSelected");
+}
+
+function selectLowPriority() {
+    unselectPriority();
+    let lowRef = document.getElementById("lowPriority")
+    lowRef.classList.add("lowPriorityButtonSelected");
+}
+
+function unselectPriority () {
+    document.getElementById("urgentPriority").classList.remove("urgentPriorityButtonSelected");
+    document.getElementById("mediumPriority").classList.remove("mediumPriorityButtonSelected");
+    document.getElementById("lowPriority").classList.remove("lowPriorityButtonSelected");
+}
