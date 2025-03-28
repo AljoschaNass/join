8b58c13266bd0c01858c47f5d8e7ececcd701cda
Stefanie Ebert <you@example.com>
2025-03-28 18:28:24 +0100

    Sign up: togglePasswordVisibility JS

--- scripts/logIn.js
+++ scripts/logIn.js
@@ -1,11 +1,29 @@
-/*Toggles Password Visibility - onclick of the eye icon; Problem: currently included as background-img*/
-/*Still missing: toggle background-image*/
-function togglePasswordVisibility() {
-    var x = document.getElementById("logInPassword");
+/*Toggles Password Visibility */
+function togglePasswordVisibility(inputId) {
+    var x = document.getElementById(inputId);
     if (x.type === "password") {
       x.type = "text";
+      var img = document.getElementById(inputId + "Img");
+      img.src = "../assets/img/icons/visibility.svg"; // Icon für Passwort sichtbar
     } else {
       x.type = "password";
+      var img = document.getElementById(inputId + "Img");
+      img.src = "../assets/img/icons/visibility_off.svg";// Icon für Passwort versteckt
     }
-    //document.getElementById("myImg").style.backgroundImage = "url(visibility.svg)";
+    
   } 
+
+    /*document.getElementById('signUpTogglePassword').addEventListener('click', function () {
+      const passwordInput = document.getElementById('password');
+      const toggleIcon = this;
+  
+      if (passwordInput.type === 'password') {
+          passwordInput.type = 'text';
+          toggleIcon.src = '../assets/img/icons/visibility.svg'; // Icon für Passwort sichtbar
+          toggleIcon.alt = 'visibility'; // Alternativtext ändern
+      } else {
+          passwordInput.type = 'password';
+          toggleIcon.src = '../assets/img/icons/visibility_off.svg'; // Icon für Passwort versteckt
+          toggleIcon.alt = 'visibility off'; // Alternativtext ändern
+      }
+  }); */
