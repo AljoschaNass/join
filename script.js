a140b5ce9483fc2505e525e752cebac439b00ab1
Stefanie Ebert <you@example.com>
2025-03-31 21:58:18 +0200

    script.js, summary.js: greeting() added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function init() {
     includeHTML();
+    greeting();
 }
 
 async function includeHTML() {
--- scripts/summary.js
+++ scripts/summary.js
@@ -12,3 +12,14 @@ function resetImage(element) {
 function switchToBoard() {
     window.location.href = '../html/board.html';
 }
+
+//greeting 
+function greeting() {
+    let d = new Date();
+    let hour = d.getHours();
+    let greetingMessage = "Good " + (hour < 12 ? "morning" : hour < 18 ? "afternoon" : "evening");
+    document.getElementById("summary_greeting_text").innerHTML = greetingMessage;
+/*if user is logged in: 
+document.getElementById("summary_greeting_text").innerHTML +=",";
+document.getElementById("summary_greeting_name").innerHTML = user.name;  */
+}
