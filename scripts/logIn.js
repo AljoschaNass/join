1af9dfd2e1b55b2cfa1bbea4026d2f4332aefe2b
Stefanie Ebert <you@example.com>
2025-04-12 16:33:48 +0200

    logIn.js path to img corrected; logIn function: error message improved;

--- scripts/logIn.js
+++ scripts/logIn.js
@@ -4,12 +4,12 @@ function togglePasswordVisibility(inputId) {
   if (x.type === "password") {
     x.type = "text";
     let img = document.getElementById(inputId + "Img");
-    img.src = "./assets/img/icons/visibility.svg"; // Icon für Passwort sichtbar
+    img.src = "../assets/img/icons/visibility.svg"; // Icon für Passwort sichtbar
     img.alt = "Password Visible";
   } else {
     x.type = "password";
     let img = document.getElementById(inputId + "Img");
-    img.src = "./assets/img/icons/visibility_off.svg";// Icon für Passwort versteckt
+    img.src = "../assets/img/icons/visibility_off.svg";// Icon für Passwort versteckt
     img.alt = "Password Hidden";
   }
 } 
@@ -19,7 +19,7 @@ function onFocus(inputId) {
   let x = document.getElementById(inputId);
   x.style.borderColor = "#29ABE2";
   let img = document.getElementById(inputId + "Img");
-  img.src = "./assets/img/icons/visibility_off.svg";// Icon für Passwort versteckt 
+  img.src = "../assets/img/icons/visibility_off.svg";// Icon für Passwort versteckt 
   img.alt = "Password Hidden";
 }
    
@@ -28,23 +28,30 @@ function onBlur(inputId) {
   let x = document.getElementById(inputId);
   x.style.borderColor = "#D9D9D9";
   let img = document.getElementById(inputId + "Img");
-  img.src = "./assets/img/icons/lock.svg";// Icon für Passwort 
+  img.src = "../assets/img/icons/lock.svg";// Icon für Passwort 
   img.alt = "Password";
   x.type = "password";
 }
 
 /*Allows the user to log in personal account*/
 async function logIn() {
-  let email = document.getElementById("logInEmail").value;
-  let password = document.getElementById("logInPassword").value;
+  let thisEmail = document.getElementById("logInEmail").value;
+  let thisPassword = document.getElementById("logInPassword").value;
   let users = await getAllUsers();
-  let user = Object.values(users).find(u => u.email === email && u.password === password);
+  let user = Object.values(users).find(u => u.email === thisEmail && u.password === thisPassword);
+  checkUser(user);
+}
+
+/*checks weather the user is registred & saves the current user to local storage or displays an error message*/
+function checkUser(user) {
   if (user) {
     saveCurrentUserToLocalStorage(user.email, user.name);
     window.location.href = "./html/summary.html";
   } else {
     document.getElementById("logIn_error").classList.remove("d_none");
-  }
+    document.getElementById("logInPassword").style.borderColor = "#ff001f";
+    document.getElementById("logInEmail").style.borderColor = "#ff001f";
+  } 
 }
 
 /*Log in for guest user*/
