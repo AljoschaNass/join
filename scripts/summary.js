1fabedb2198f2920b9489dc8b690f4fb5c7fa8c8
Stefanie Ebert <steffi.123@posteo.net>
2025-04-22 16:40:34 +0200

    load number of tasks in summary

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,5 +1,6 @@
 function initSummary() {
     init();
+    loadNumberOfTasks();
     greeting();
 }
 
@@ -35,4 +36,40 @@ function greeting() {
         document.getElementById("summary_greeting_text").innerHTML = greetingMessage + ",";
         document.getElementById("summary_greeting_name").innerHTML = currentUserName;
     }
+}
+
+
+//Filter tasks for summary
+function loadNumberOfTasksWithStatus(tasks, status) {
+    let filteredTasks = tasks.filter(task => task.status === status);
+    return filteredTasks.length;
+}
+
+
+function loadNumberOfTasksWithPriority(tasks, priority) {
+    let filteredTasks = tasks.filter(task => task.priority === priority);
+    return filteredTasks.length;
+}
+
+
+//load tasks for summary
+async function loadNumberOfTasks() {
+    let tasks = await getAllTasks();
+    let tasksArray = Object.values(tasks);
+    document.getElementById("summary_number_of_to_do_tasks").innerHTML = loadNumberOfTasksWithStatus(tasksArray, 'toDoTask');
+    document.getElementById("summary_number_of_in_progress_tasks").innerHTML = loadNumberOfTasksWithStatus(tasksArray, 'inProgressTask');
+    document.getElementById("summary_number_of_done_tasks").innerHTML = loadNumberOfTasksWithStatus(tasksArray, 'doneTask');
+    document.getElementById("summary_number_of_await_feedback_tasks").innerHTML = loadNumberOfTasksWithStatus(tasksArray, 'awaitFeedbackTask');
+    document.getElementById("summary_number_of_urgent_tasks").innerHTML = loadNumberOfTasksWithPriority(tasksArray, 'urgent');
+    document.getElementById("summary_number_of_tasks_in_board").innerHTML = sumOfOpenTasks(tasksArray);
+}
+
+
+//Sum of all open tasks
+function sumOfOpenTasks(tasksArray) {
+    let toDoTasks = loadNumberOfTasksWithStatus(tasksArray, 'toDoTask');
+    let inProgressTasks = loadNumberOfTasksWithStatus(tasksArray, 'inProgressTask');
+    let awaitFeedbackTasks = loadNumberOfTasksWithStatus(tasksArray, 'awaitFeedbackTask');
+    let sum = toDoTasks + inProgressTasks + awaitFeedbackTasks;
+    return sum;
 }
