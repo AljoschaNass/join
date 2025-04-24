62306dbee84fa72ae2a2f7ddd7c967a4208c4730
Stefanie Ebert <steffi.123@posteo.net>
2025-04-25 00:12:47 +0200

    load tasks after search

--- scripts/search.js
+++ scripts/search.js
@@ -3,5 +3,33 @@ async function searchTask() {
     let allTasks = await getAllTasks();
     let allTasksArray = Object.values(allTasks);
     let filteredTasks = allTasksArray.filter(task => task.title.toLowerCase().includes(searchInput) || task.description.toLowerCase().includes(searchInput));
-    console.log(filteredTasks);
+    if (filteredTasks.length > 0) {
+         loadTasksBoardAfterSearch(filteredTasks);
+    } else {
+        alert('No tasks found!');//muss noch schöner
+    }
+}
+
+
+function loadTasksBoardAfterSearch(tasks){
+    let columns = {
+        "toDoTask": document.getElementById('toDoTask'),
+        "inProgressTask": document.getElementById('inProgressTask'),
+        "awaitFeedbackTask": document.getElementById('awaitFeedbackTask'),
+        "doneTask": document.getElementById('doneTask')
+    };
+    for (let key in columns) {
+        columns[key].innerHTML = '';
+    }
+    for (let taskId in tasks) {
+        let task = tasks[taskId];
+        if (columns[task.status]) {
+            columns[task.status].innerHTML += renderTaskCard(task.assignedTo, task.category, task.description, task.dueDate, task.priority, task.subtasks, task.title, taskId);
+        }
+    }
+    for (let status in columns) {
+        if (columns[status].innerHTML === '') {
+            columns[status].innerHTML = renderNoTaskCard(status);
+        }
+    }
 }
